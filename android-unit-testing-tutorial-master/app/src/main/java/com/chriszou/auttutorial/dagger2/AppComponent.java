9f1113ac054e08ff50ce75532dba4257c844239c
ansqun <ansqun@qq.com>
2016-11-07 21:39:12 +0800

    Initial commit

---
+++ android-unit-testing-tutorial-master/app/src/main/java/com/chriszou/auttutorial/dagger2/AppComponent.java
@@ -0,0 +1,18 @@
+package com.chriszou.auttutorial.dagger2;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+/**
+ * Created by xiaochuang on 5/14/16.
+ */
+@Component(modules = {AppModule.class})
+@Singleton
+public interface AppComponent {
+    LoginPresenter loginPresenter();
+
+    void inject(LoginActivity mainActivity);
+
+    void inject(com.chriszou.auttutorial.daggermock.LoginActivity loginActivity);
+}
